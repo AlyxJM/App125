b51c2c39b9fcf79f30e3ec818c98d6ed325fa434
AlyxJM <merritt9@illinois.edu>
2018-12-05 20:46:05 -0600

    Start working with final project API

--- app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
@@ -5,15 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -42,28 +43,27 @@ public final class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Attach the handler to our UI button
-        final Button startAPICall = findViewById(R.id.startAPICall);
+        final Button startAPICall = findViewById(R.id.generateRandomPair);
         startAPICall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                Log.d(TAG, "Start API button clicked");
-                startAPICall();
+                Log.d(TAG, "Image button clicked");
+                startImageAPICall();
+                startQuoteAPICall();
             }
         });
-
-        // Make sure that our progress bar isn't spinning and style it a bit
-        ProgressBar progressBar = findViewById(R.id.progressBar);
-        progressBar.setVisibility(View.INVISIBLE);
     }
 
     /**
-     * Make an API call.
+     * Make image API call.
      */
-    void startAPICall() {
+    void startImageAPICall() {
         try {
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                     Request.Method.GET,
-                    "",
+                    "https://api.unsplash.com/photos/random"
+                            + "?client_id="
+                            + "4a89728ee71c2b4687f2a18a3d029e7501b74003ad109a818ee170112582ef7e",
                     null,
                     new Response.Listener<JSONObject>() {
                         @Override
@@ -75,8 +75,37 @@ public final class MainActivity extends AppCompatActivity {
                         public void onErrorResponse(final VolleyError error) {
                             Log.w(TAG, error.toString());
                         }
-                    });
+                    }) {
+            };
             requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    /**
+     * Make quote API call.
+     */
+    void startQuoteAPICall() {
+        try {
+            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                    Request.Method.GET,
+                    "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+                    null,
+                    new Response.Listener<JSONArray>() {
+                        @Override
+                        public void onResponse(final JSONArray response) {
+                            Log.d(TAG, response.toString());
+
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(final VolleyError error) {
+                            Log.w(TAG, error.toString());
+                        }
+                    }) {
+                };
+            requestQueue.add(jsonArrayRequest);
         } catch (Exception e) {
             e.printStackTrace();
         }
